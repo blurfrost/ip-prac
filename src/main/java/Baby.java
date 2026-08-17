a4c19dfc70a967013435fdb052a383b85a5b0a7e
blurfrost <ksyc07@gmail.com>
2026-08-17 17:41:03 +0800

    Replace Duke with Baby ASCII art and update references
    Rename main class from Duke.java to Baby.java, update README.md and docs/README.md to reference 'Baby' instead of 'Duke', and include the boxed-style ASCII banner for Baby

---
+++ src/main/java/Baby.java
@@ -0,0 +1,10 @@
+public class Baby {
+    public static void main(String[] args) {
+        String banner = " ____    _    ____ __   __\n"
+                + "| __ )  / \\  | __ )\\ \\ / /\n"
+                + "|  _ \\ / _ \\ |  _ \\ \\ V /\n"
+                + "| |_) / ___ \\| |_) | | |\n"
+                + "|____/_/   \\_\\____/  |_|\n";
+        System.out.println(banner);
+    }
+}
